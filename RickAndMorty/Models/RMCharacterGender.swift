d210baab5827fa5526d052d9f08363c3eda547e9
Sherzod <mirzaevsherzodbek1995@gmail.com>
2023-01-17 13:56:16 +0500

    Creating the Models the API spec of the REST API with Coddle

---
+++ RickAndMorty/Models/RMCharacterGender.swift
@@ -0,0 +1,16 @@
+//
+//  RMCharacterGender.swift
+//  RickAndMorty
+//
+//  Created by Sherzod on 17/01/23.
+//
+
+import Foundation
+
+enum RMCharacterGender: String, Codable {
+    case male = "Male"
+    case female = "Female"
+    case genderless = "Genderless"
+    case `unknown` = "unknown"
+    
+}
---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Sherzod on 17/01/23.
+//
+
+import Foundation
+
+struct RMOrigin: Codable {
+    let namre: String
+    let url: String
+}
