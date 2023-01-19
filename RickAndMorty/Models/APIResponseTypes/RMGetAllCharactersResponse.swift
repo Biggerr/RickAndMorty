81df597717acd9dc1e513b36c3a78fb489be5a13
Sherzod <mirzaevsherzodbek1995@gmail.com>
2023-01-19 19:58:10 +0500

    Pagination , Hashable and Caching mechanism.

---
+++ RickAndMorty/Models/APIResponseTypes/RMGetAllCharactersResponse.swift
@@ -0,0 +1,22 @@
+//
+//  RMGetAllCharactersResponse.swift
+//  RickAndMorty
+//
+//  Created by Sherzod on 17/01/23.
+//
+
+import Foundation
+
+struct RMGetAllCharactersResponse: Codable {
+    struct Info: Codable {
+        let count: Int
+        let pages: Int
+        let next: String?
+        let prev: String?
+    }
+    let info: Info
+    let results: [RMCharacter]
+}
+
+
+
--- RickAndMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
+++ RickAndMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
@@ -7,10 +7,11 @@
 
 import Foundation
 
-final class RMCharacterListViewCellViewModel {
+final class RMCharacterCollectionViewCellViewModel: Hashable, Equatable {
     public let characterName: String
     private let characterStatus: RMCharacterStatus
     private let characterImageUrl: URL?
+    
     
     // MARK: - Init
     
@@ -34,14 +35,18 @@ final class RMCharacterListViewCellViewModel {
             completion(.failure(URLError(.badURL)))
             return
         }
-        let request = URLRequest(url: url)
-        let task = URLSession.shared.dataTask(with: request) { data, _, error in
-            guard let data = data, error == nil else {
-                completion(.failure(error ?? URLError(.badServerResponse)))
-                return
-            }
-            completion(.success(data))
-        }
-        task.resume()
+        RMImageLoader.shared.downloadImage(url, completion: completion)
+    }
+    
+    // MARK: - Hashable
+    
+    static func == (lhs: RMCharacterCollectionViewCellViewModel, rhs: RMCharacterCollectionViewCellViewModel) -> Bool {
+        return lhs.hashValue == rhs.hashValue
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(characterName)
+        hasher.combine(characterStatus)
+        hasher.combine(characterImageUrl)
     }
 }
